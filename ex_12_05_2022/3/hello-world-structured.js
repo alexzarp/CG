0e1e53fbb9e20fae0da3e9f191c8771ef9389af0
alexzarp <alexszds@gmail.com>
2022-06-02 15:24:45 -0300

    Updatade da tristeza mentira

--- ex_12_05_2022/3/hello-world-structured.js
+++ ex_12_05_2022/3/hello-world-structured.js
@@ -5,16 +5,47 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshBasicMaterial({color: 0xff0000});
-var cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+var cubes = new THREE.Group();
+
+// for (let i = 0; i < 10; i++) {
+//     var cube = new THREE.Mesh(geometry, material);
+//     // cube.position.x += i;
+//     cubes.add(cube);
+// }
+// -------------------------------------
+var material = new THREE.MeshBasicMaterial({color: 0x836FFF});
+var geometry = new THREE.BoxGeometry(4, 2, 1);
+
+var car = new THREE.Mesh(geometry, material);
+cubes.add(car);
+
+var material = new THREE.MeshBasicMaterial({color: 0xFFFFFF});
+var geometry = new THREE.BoxGeometry(1, 5, 1);
+
+var wheel = new THREE.Mesh(geometry, material);
+cubes.add(wheel);
+// -------------------------------------
+
+wheel.scale.y = 0.2;
+wheel.position.y = -1.1;
+wheel.position.x = 1.3;
+wheel.position.z = 0.1;
+
+for (let i = 0; i < 4; i++) {
+
+}
+
+// wheel.rotation.z = 80;
+// wheel.rotation.y = 50;
+// wheel.position.x = 30;
+
+scene.add(cubes);
 
 camera.position.z = 5;
 
-cube.rotation.x = 5.0;
-cube.rotation.y = 3.0;
-cube.position.x = 2.0;
+// cube.rotation.x = 5.0;
+// cube.rotation.y = 3.0;
+// cube.position.x = 2.0;
 
 var velocityup = new THREE.Vector3(0, 0.1, 0);
 var velocitydown = new THREE.Vector3(0, -0.1, 0);
@@ -26,7 +57,8 @@ velocity = velocityup;
 var animate = function() {
     requestAnimationFrame(animate);
 
-    cube.position.add(velocity);
+    // cubes.rotation.z += 0.1;
+    // cubes.rotation.y += 0.05;
 
     renderer.render(scene, camera);
 };
